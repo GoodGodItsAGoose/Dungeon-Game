9ce3a74a1191322f6fff8d0182341d6e916a398d
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2025-03-12 17:57:42 -0700

    Update Save.py
    Notes:
    - Patched some bugs

--- Save.py
+++ Save.py
@@ -2,7 +2,7 @@ import Info as i
 
 # loading the last save's info from saves.txt
 def loadSave():
-    subjects = ["damage-pot", "dodge", "restoration", "charge", "uses", "name", "state", "modifier"]
+    subjects = ["damage-pot", "dodge", "restoration", "charge", "recharges", "name", "state", "modifier"]
     results = {}
     with open('DungeonGame/saves.txt') as file:
         for line in file:
@@ -10,17 +10,24 @@ def loadSave():
                 name, *grades = line.strip().split(', ')
                 results[name] = dict(zip(subjects, grades))
     i.playerPotions = results[str(len(results)-1)]
-    i.playerItems["charge"] = results[str(len(results)-1)]["charge"]
-    i.playerItems["uses"] = results[str(len(results)-1)]["uses"]
-    i.playerItems["name"] = results[str(len(results)-1)]["name"]
-    i.playerItemsState = results[str(len(results)-1)]["state"]
-    i.playerItemModifier = results[str(len(results)-1)]["modifier"]
+    if results[str(len(results)-1)]["charge"] == "None":
+        i.playerItems["charge"] = None
+        i.playerItems["recharges"] = None
+        i.playerItems["name"] = None
+        i.playerItemsState = False
+        i.playerItemModifier = None
+    else:
+        i.playerItems["charge"] = results[str(len(results)-1)]["charge"]
+        i.playerItems["recharges"] = results[str(len(results)-1)]["recharges"]
+        i.playerItems["name"] = results[str(len(results)-1)]["name"]
+        i.playerItemsState = results[str(len(results)-1)]["state"]
+        i.playerItemModifier = results[str(len(results)-1)]["modifier"]
 
 # updating the save's files
 def updateSave():
-    saveStats = i.playerPotions, i.playerItems, i.playerItemsState, i.playerItemModifier
-    removes = ["damage-pot", "dodge", "restoration", "charge", "uses", "name", "state", "modifier", "damage", "{", "}", ": ", '"', "'", "(", ")"]
-    subjects = ["damage-pot", "dodge", "restoration", "charge", "uses", "name", "state", "modifier"]
+    saveStats = i.playerPotions
+    removes = ["damage-pot", "dodge", "restoration", "charge", "recharges", "name", "state", "modifier", "damage", "{", "}", ": ", '"', "'", "(", ")", "res"]
+    subjects = ["damage-pot", "dodge", "restoration", "charge", "recharges", "name", "state", "modifier"]
     num = 0
     results = {}
     with open('DungeonGame/saves.txt') as file:
@@ -29,7 +36,7 @@ def updateSave():
                 name, *grades = line.strip().split(', ')
                 results[name] = dict(zip(subjects, grades))
     number = len(results)
-    for _ in range(16):
+    for _ in range(17):
         saveStats = str(saveStats).replace(removes[num], "")
         num += 1
     with open("DungeonGame/saves.txt", "a") as f:
