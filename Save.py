526899dbbbb0d4dbf8c64a220993d42c4353e8f0
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2025-03-12 20:52:34 -0700

    Update Save.py
    Notes:
    - Fixed bugs

--- Save.py
+++ Save.py
@@ -9,7 +9,9 @@ def loadSave():
             if line != '\n':  # Skip empty lines
                 name, *grades = line.strip().split(', ')
                 results[name] = dict(zip(subjects, grades))
-    i.playerPotions = results[str(len(results)-1)]
+    i.playerPotions["damage-pot"] = int(results[str(len(results)-1)]["damage-pot"])
+    i.playerPotions["dodge"] = int(results[str(len(results)-1)]["dodge"])
+    i.playerPotions["restoration"] = int(results[str(len(results)-1)]["restoration"])
     if results[str(len(results)-1)]["charge"] == "None":
         i.playerItems["charge"] = None
         i.playerItems["recharges"] = None
@@ -17,15 +19,16 @@ def loadSave():
         i.playerItemsState = False
         i.playerItemModifier = None
     else:
-        i.playerItems["charge"] = results[str(len(results)-1)]["charge"]
-        i.playerItems["recharges"] = results[str(len(results)-1)]["recharges"]
+        i.playerItems["charge"] = int(results[str(len(results)-1)]["charge"])
+        i.playerItems["recharges"] = int(results[str(len(results)-1)]["recharges"])
         i.playerItems["name"] = results[str(len(results)-1)]["name"]
         i.playerItemsState = results[str(len(results)-1)]["state"]
-        i.playerItemModifier = results[str(len(results)-1)]["modifier"]
+        i.playerItemModifier = int(results[str(len(results)-1)]["modifier"])
 
 # updating the save's files
 def updateSave():
-    saveStats = i.playerPotions
+    saveStats = i.playerPotions, i.playerItems, i.playerItemsState, i.playerItemModifier
+    saveStats = str(saveStats).replace("'charge': '', 'recharges': 'None', 'name': 'None', 'state': 'None', 'modifier': 'False'}, {", "")
     removes = ["damage-pot", "dodge", "restoration", "charge", "recharges", "name", "state", "modifier", "damage", "{", "}", ": ", '"', "'", "(", ")", "res"]
     subjects = ["damage-pot", "dodge", "restoration", "charge", "recharges", "name", "state", "modifier"]
     num = 0
